7674e25e02f948088c64d27a2e8c4e2f9bcf1a21
onaboat <onaboatatsea99@gmail.com>
2026-04-13 15:12:27 +1000

    feat(config): add partners data config

---
+++ lib/config/partners.ts
@@ -0,0 +1,17 @@
+export interface Partner {
+  id: string;
+  name: string;
+  logo: string;
+  url: string;
+}
+
+export const PARTNERS: Partner[] = [
+  // Populated when partner logos are provided
+  // Example:
+  // {
+  //   id: "solana",
+  //   name: "Solana",
+  //   logo: "/partners/solana.svg",
+  //   url: "https://solana.com",
+  // },
+];
